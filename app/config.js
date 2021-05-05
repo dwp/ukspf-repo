7ff62dcaeef749a283aed45dd7e0ce91d3f6b3e3
GDS-RO <richard.odwyer@engineering.digital.dwp.gov.uk>
2021-05-05 14:38:11 +0100

    initial project setup changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Shared Prosperity Fund ',
+  serviceName: 'Shared Prosperity Fund',
 
   // Default port that prototype runs on
   port: '3000',
