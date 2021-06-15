6bb0c80d57ea39d007dce4275e10d59518970d9f
richard.odwyer <richard.odwyer@engineering.digital.dwp.gov.uk>
2021-06-15 10:17:25 +0100

    Routing added and general tidy-up

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,46 @@
 const express = require('express')
 const router = express.Router()
-
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
 // Add your routes here - above the module.exports line
+
+// Branching
+router.post('/branch-eligibility-1', function (req, res) {
+  // Eligibility-1 branching
+
+  const regcheck = req.session.data['invitation-check']
+
+  if (regcheck === 'false') {
+    res.redirect('version-1/eligibility-1-no')
+  } else {
+    res.redirect('version-1/eligibility-2')
+  }
+})
+
+router.post('/branch-eligibility-2', function (req, res) {
+  // Eligibility-2 branching
+
+  const bankcheck = req.session.data['bank-check']
+
+  if (bankcheck === 'false') {
+    res.redirect('version-1/eligibility-2-no')
+  } else {
+    res.redirect('version-1/eligibility-3')
+  }
+})
+
+router.post('/branch-eligibility-3', function (req, res) {
+  // Eligibility-3 branching
+
+  const chcheck = req.session.data['ch-check']
+
+  if (chcheck === 'false') {
+    res.redirect('version-1/eligibility-3-no')
+  } else {
+    res.redirect('version-1/how-much-money-are-you-applying-for')
+  }
+})
+
+
 
 module.exports = router
