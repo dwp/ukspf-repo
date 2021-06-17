59019755506cadeadd6a6c8d90adfc37c67ba896
richard.odwyer <richard.odwyer@engineering.digital.dwp.gov.uk>
2021-06-17 16:06:57 +0100

    config change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a Shared Prosperity Fund grant',
+  serviceName: 'Apply for a UK Shared Prosperity Fund grant',
 
   // Default port that prototype runs on
   port: '3000',
