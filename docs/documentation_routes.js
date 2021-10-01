919cfe1fd5cf737f338b80a02323fbeee503e226
richard.odwyer <richard.odwyer@engineering.digital.dwp.gov.uk>
2021-10-01 16:08:08 +0100

    Added S&R email step. Reordered pages. Updated Check your answers.

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -74,7 +74,35 @@ router.post('/examples/branching/over-18-answer', function (req, res) {
   if (over18 === 'false') {
     res.redirect('/version-2/return-to-an-application')
   } else {
-    res.redirect('/version-2/start-a-new-application')
+    res.redirect('/version-2/invitation-number')
+  }
+})
+
+router.post('/examples/branching', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const over18 = req.session.data['over-18']
+
+  if (over18 === 'false') {
+    res.redirect('/version-2a/return-to-an-application')
+  } else {
+    res.redirect('/version-2a/invitation-number')
+  }
+})
+
+router.post('/examples/branching-version-2b', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const over18 = req.session.data['over-18']
+
+  if (over18 === 'false') {
+    res.redirect('/version-2b/return-to-an-application')
+  } else {
+    res.redirect('/version-2b/start-a-new-application')
   }
 })
 
